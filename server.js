3565abdb1a4292e98ecedf8edcc4141c882564bb
John Michael Laceda <john.laceda@gmail.com>
2019-07-24 12:43:07 -0700

    make routes work

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +13,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// Define routes here
+app.use(routes)
 
 // Send every other request to the React app
 // Define any API routes before this runs

4b90f21a6cf26fd284b18f44640a2332c987c5a8
Vani <vani.karikalan@gmail.com>
2019-07-24 23:31:49 -0700

    initial commit for register business profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import BizProfile from "./components/BizProfile";
 import './components/Navbar/index';
 import "./App.css";
 import Navbar from "./components/Navbar/index";
@@ -9,6 +10,9 @@ class App extends Component {
     return (
       <Router>
         <Navbar />
+        <Switch>
+        <Route path='/newProfile' component={BizProfile} />
+      </Switch>
       </Router>
     );
   }

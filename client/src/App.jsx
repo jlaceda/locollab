0d920cacbd6299478d630eeb5230d9b269a6c744
John Michael Laceda <john.laceda@gmail.com>
2019-07-25 20:23:02 -0700

    fix merge conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,26 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import BizProfile from "./components/BizProfile"
-import './components/Navbar/index';
-import "./App.css";
-import Navbar from "./components/Navbar/index";
-import Main from "./components/Main";
-<<<<<<< HEAD
-import Profile from "./components/ProfilePage/profile"
-=======
-import Cards from "./components/Cards/cards"
->>>>>>> 158f6b838bb61b2ebbe3dcf815d7b55e8db00ffb
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import BizProfile from './components/BizProfile'
+import './components/Navbar/index'
+import './App.css'
+import Navbar from './components/Navbar/index'
+import Main from './components/Main'
+import Profile from './components/ProfilePage/profile'
+import Cards from './components/Cards/cards'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <Router>
         <Navbar />
         <Switch>
-        <Route path='/profile' components={Profile}/>
-        <Route path='/newProfile' component={BizProfile}/>
-        <Route path='/' component={Cards}/>
-      </Switch>
+          <Route path='/profile' components={Profile} />
+          <Route path='/newProfile' component={BizProfile} />
+          <Route path='/' component={Cards} />
+        </Switch>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App

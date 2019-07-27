0f16fa9112bb371a3ed6fb8c4f0941566758608d
John Michael Laceda <john.laceda@gmail.com>
2019-07-27 11:29:30 -0700

    fix profilepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import BizProfile from './components/BizProfile'
-import './components/Navbar/index'
 import './App.css'
-import Navbar from './components/Navbar/index'
+import Navbar from './components/Navbar'
 import Home from './components/Home/home'
-import Profile from './components/ProfilePage/profile'
+import Profile from './components/Profile'
 
 class App extends Component {
   render () {
@@ -13,7 +12,7 @@ class App extends Component {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/profile' components={Profile} />
+          <Route path='/profile' component={Profile} />
           <Route path='/newProfile' component={BizProfile} />
           <Route path='/' component={Home} />
         </Switch>
--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import "./style.css";
-
-
 
 function Profile() {
 return(

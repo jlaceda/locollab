06914f30e4587ede5fe93829125753ee1a3cbde4
KevinL <mastahkevin@gmail.com>
2019-07-28 21:22:24 -0700

    Removed checkbox.

--- client/src/components/Auth/login.jsx
+++ client/src/components/Auth/login.jsx
@@ -15,10 +15,6 @@ function Login () {
                               <label for="exampleInputPassword1">Password</label>
                               <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
                          </div>
-                         <div className="form-group form-check">
-                              <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                              <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                         </div>
                          <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
                </div>

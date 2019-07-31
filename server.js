4bcc854935422a4cf73e7f2c88d0f56e77113d8f
John Michael Laceda <john.laceda@gmail.com>
2019-07-30 19:23:07 -0700

    explicit static paths

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 }
 
 // Define routes here
@@ -19,7 +19,7 @@ app.use(routes)
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/locollab", {useNewUrlParser: true});

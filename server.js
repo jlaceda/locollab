4440ea8dd79bf488dff01e64f9f946e9c6013499
John Michael Laceda <john.laceda@gmail.com>
2019-07-30 19:57:05 -0700

    comment out direct route index

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(routes)
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/locollab", {useNewUrlParser: true});
 

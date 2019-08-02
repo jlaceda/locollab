5d022766c8a06b82ae7388e4d82b2382434f5fb1
John Michael Laceda <john.laceda@gmail.com>
2019-08-01 20:48:14 -0700

    connect Home page to getProfiles api call

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,55 +1,47 @@
 import React from "react";
 import "./style.css";
 import StockPhoto from "./bgsmall.png"
+import API from "../../utils/API";
 
-function Home() {
-return(
-<div class="container">
-    <div class="jumbotron">
-        <h1 class="display-4">Businesses in Seattle, WA</h1>
-    </div>
+class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            profiles: []
+        };
+        API.getProfiles().then(res => {
+            this.setState({
+                profiles: res.data.profiles
+            });
+        })
+    }
 
-    
-    <div class="media">
-        <img src={StockPhoto} class="mr-3" alt="..."></img>
-         <div class="media-body">
-            <h2 class="mt-0">Jamba Juice</h2>
-            <p>Smoothies and Juices</p>
-        </div>
-    </div>
-    <div class="media">
-        <img src={StockPhoto} class="mr-3" alt="..."></img>
-         <div class="media-body">
-            <h2 class="mt-0">Jamba Juice</h2>
-            <p>Smoothies and Juices</p>
-        </div>
-    </div>
-    <div class="media">
-        <img src={StockPhoto} class="mr-3" alt="..."></img>
-         <div class="media-body">
-            <h2 class="mt-0">Jamba Juice</h2>
-            <p>Smoothies and Juices</p>
-        </div>
-    </div>
-    <div class="media">
-        <img src={StockPhoto} class="mr-3" alt="..."></img>
-         <div class="media-body">
-            <h2 class="mt-0">Jamba Juice</h2>
-            <p>Smoothies and Juices</p>
-        </div>
-    </div>
-    <div class="media">
-        <img src={StockPhoto} class="mr-3" alt="..."></img>
-         <div class="media-body">
-            <h2 class="mt-0">Jamba Juice</h2>
-            <p>Smoothies and Juices</p>
-        </div>
-    </div>
+    render() {
+        return (
+            <div class="container">
+                <div class="jumbotron">
+                    <h1 class="display-4">Businesses in Seattle, WA</h1>
+                </div>
 
-</div>
+                {
+                this.state.profiles.map(profile => {
+                    return (
+                        <div class="media" business-id={profile._id}>
+                            <img src={StockPhoto} class="mr-3" alt="..."></img>
+                            <div class="media-body">
+                                <h2 class="mt-0">{profile.name}</h2>
+                                <p>{profile.description}</p>
+                                <p>{profile.location}</p>
+                                <p>{profile.category}</p>
+                            </div>
+                        </div>
+                    )
+                })
+                }
 
-
-)
+            </div>
+        )
+    }
 }
 
 export default Home;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,5 +4,8 @@ export default {
     // Add New Profile
     addProfile: function(profile) {
       return axios.post("/api/profile", profile);
+    },
+    getProfiles: function() {
+      return axios.get("/api/profiles");
     }
 };

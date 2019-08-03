cb6b798cca7b7af581e5fd1784eafd5a61453b63
Vani <vani.karikalan@gmail.com>
2019-08-03 13:47:38 -0700

    added route to userprofile form add profile and changed structure to user profile

--- client/src/components/UserProfile/index.jsx
+++ client/src/components/UserProfile/index.jsx
@@ -1,63 +1,77 @@
 import React from "react";
 import "./style.css";
-import photo from "../UserProfile/bgsmall.png"
+import photo from "../UserProfile/bgsmall.png";
+import API from "../../utils/API";
 
+class UserProfile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // profiles: []
+    };
+    API.specificProfile().then(res => {
+      this.setState({
+        profile: res.data.profile
+      });
+    })
+  }
 
-function UserProfile() {
-return(
-<div clasName="container">
-<div className="row" id="head">
-<div className="jumbotron">
-    
-  <div className="row">
-  <div className="col-4">
-  <img src={photo} className="mr-3" alt="..."></img>
-  </div>
-  <div className="col-8">
-  <h1 className="display-4">Business Name <a href='/editProfile'> <button id="editBtn" type="button" class="btn btn-secondary btn-sm"><h5>Edit Profile</h5></button></a></h1>
-  <h4>Contact Us:</h4>
-  <h5>Phone: </h5>
-  <h5>Email: </h5>
-  <h5>www.examplewebsite.com</h5>
-  </div>
-  </div>
-</div>
-</div>
+  render() {
+    return (
+      <div clasName="container">
+        <div className="row" id="head">
+          <div className="jumbotron">
 
-<div className="row" id="body">
-  
-  <div className="row" id="header">
-   <h3>Posts by 'businessname'</h3>
-  </div>
-  <div className="row" id="content">
-  <div className="card">
-    <div className="card-body">
-      
-      <p className="card-title">Free Samples
+            <div className="row">
+              <div className="col-4">
+                <img src={photo} className="mr-3" alt="..."></img>
+              </div>
+              <div className="col-8">
+                <h1 className="display-4">Business Name <a href='/editProfile'> <button id="editBtn" type="button" class="btn btn-secondary btn-sm"><h5>Edit Profile</h5></button></a></h1>
+                <h4>Contact Us:</h4>
+                <h5>Phone: </h5>
+                <h5>Email: </h5>
+                <h5>www.examplewebsite.com</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row" id="body">
+
+          <div className="row" id="header">
+            <h3>Posts by 'businessname'</h3>
+          </div>
+          <div className="row" id="content">
+            <div className="card">
+              <div className="card-body">
+
+                <p className="card-title">Free Samples
       <span className="date">6/17/19 2:30pm Seattle, WA</span>
-      <span><button type="button" class="btn btn-secondary btn-sm">Edit Post</button></span>
-      </p>
-      
-      <p className="card-text">We would like to provide samples of our newest summer smoothies to businesses in the area.</p>
-      <button className="btn btn-primary">More Info</button>
-    </div>
-  </div>
-  <div className="card">
-    <div className="card-body">
-      
-      <p className="card-title">Free Samples
-      <span className="date">6/17/19 2:30pm Seattle, WA</span>
-      <span><button type="button" class="btn btn-secondary btn-sm">Edit Post</button></span>
-      </p>
-      
-      <p className="card-text">We would like to provide samples of our newest summer smoothies to businesses in the area.</p>
-      <button className="btn btn-primary">More Info</button>
-    </div>
-  </div>
-</div>
-</div>
-</div>
-)
-}
+                  <span><button type="button" class="btn btn-secondary btn-sm">Edit Post</button></span>
+                </p>
 
-export default UserProfile;
+                <p className="card-text">We would like to provide samples of our newest summer smoothies to businesses in the area.</p>
+                <button className="btn btn-primary">More Info</button>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-body">
+
+                <p className="card-title">Free Samples
+      <span className="date">6/17/19 2:30pm Seattle, WA</span>
+                  <span><button type="button" class="btn btn-secondary btn-sm">Edit Post</button></span>
+                </p>
+
+                <p className="card-text">We would like to provide samples of our newest summer smoothies to businesses in the area.</p>
+                <button className="btn btn-primary">More Info</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
+
+  export default UserProfile;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,7 +9,12 @@ export default {
     editProfile: function(id) {
       return axios.put("/api/profile/" + id);
     },
+    // All profiles in home page
     getProfiles: function() {
       return axios.get("/api/profiles");
+    },
+    //Specific Profile with id
+    specificProfile: function(id) {
+      return axios.get("/api/profile", + id);
     }
 };

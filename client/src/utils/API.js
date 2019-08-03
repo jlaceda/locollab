ecf82e10092822f9d45f94cb08ba364c784b0f58
Vani <vani.karikalan@gmail.com>
2019-08-03 12:11:18 -0700

    accept

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,12 +4,5 @@ export default {
     // Add New Profile
     addProfile: function(profile) {
       return axios.post("/api/profile", profile);
-    },
-    // Edit Profile
-    editProfile: function(id) {
-      return axios.put("/api/profile/" + id);
-    },
-    getProfiles: function() {
-      return axios.get("/api/profiles");
     }
 };

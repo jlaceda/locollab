9dde1e6e388aeda156231289d5facdb20399aaa2
John Michael Laceda <john.laceda@gmail.com>
2019-08-08 04:26:33 -0700

    spelling

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -10,7 +10,7 @@ class Login extends Component {
           this.state = {
                username: '',
                password: '',
-               reidrectTo: null,
+               redirectTo: null,
                loggedIn: false,
                businessId: "",
                userId: "",
